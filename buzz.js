8e72f5746ec4e30e4ff8a1eab3a6ff981d5ce8d9
bkmunoz <82331185+bkmunoz@users.noreply.github.com>
2021-04-28 11:59:25 -0500

    Corrections

--- buzz.js
+++ buzz.js
@@ -1,8 +1,7 @@
 var num;
 
-for (num = 1; num <= 99; num++) {
-    console.log(num);
-if (num % 3 == 0 && num % 5 == 0 ) {
+for (num = 1; num <= 100; num++) {
+    if (num % 3 == 0 && num % 5 == 0 ) {
     console.log("FizzBuzz");
 }
 else if (num % 3 == 0) {
